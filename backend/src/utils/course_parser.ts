cf575708b3f2e4e04bcd982a61f2c114112a18b8
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2025-03-22 18:00:31 -0400

    added comment

--- backend/src/utils/course_parser.ts
+++ backend/src/utils/course_parser.ts
@@ -1,7 +1,11 @@
 const extractCourseHistory = (text: string): string => {
   const regexPatterns =
     /\b(Spring|Fall|A|A-|B\+|B|B-|C\+|C|C-|D\+|D|F|IF|INC|__|AUD|CR|DR|IP|NR|P|SAT|W|WF|WP|Y)\b/;
+  // see https://www.umass.edu/registrar/grading-system-gpa-calculation
+  // added Spring and Fall for semester parsing. 
 
+  //TODO: will probably need to change regex to use course codes like CHEM or COMPSCI instead of grades 
+  //This would solve the in progress course parsing issue
   const lines = text.split("\n");
   console.log(text);
 
