8409c43578cbdb5ebdd0db361e9aef9d49e96f0a
Srinanda Yallapragada <srinanda.yallapragada@gmail.com>
2025-03-22 17:58:41 -0400

    Course parser in progress. Currently the parser can find most coures. It cannot find In Progress coureses yet which is to be implemented

--- backend/src/utils/course_parser.ts
+++ backend/src/utils/course_parser.ts
@@ -1,6 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const extractCourseHistory = (text: string): string => {
-  return "Courses parsed";
+  const regexPatterns =
+    /\b(Spring|Fall|A|A-|B\+|B|B-|C\+|C|C-|D\+|D|F|IF|INC|__|AUD|CR|DR|IP|NR|P|SAT|W|WF|WP|Y)\b/;
+
+  const lines = text.split("\n");
+  console.log(text);
+
+  const extractedLines: string[] = [];
+
+  for (const line of lines) {
+    if (regexPatterns.test(line)) {
+      extractedLines.push(line);
+    }
+  }
+  return extractedLines.join("\n");
 };
 
 export default extractCourseHistory;
