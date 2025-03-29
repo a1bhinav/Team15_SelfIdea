e574350bc8ac777290045b144c4cb0ceeef1a393
ananyasrivastav12 <ananyasrivas01@gmail.com>
2025-03-28 22:30:27 -0400

    updated transcript upload page

--- frontend/src/components/TranscriptUploadPage.tsx
+++ frontend/src/components/TranscriptUploadPage.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from './NavBar'; 
 import './TranscriptUploadPage.css';
 
 const TranscriptUploadPage: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const navigate = useNavigate();
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -13,23 +15,25 @@ const TranscriptUploadPage: React.FC = () => {
     }
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    //need to connect this to pdf parser
+    navigate('/transcript-review');
+  };
+
   return (
     <div className="transcript-upload-container">
       <NavBar />
       <main className="transcript-content">
         <div className="centered-content">
-
           <h1 className="transcript-title">
             Drop your <span className="red-text">transcript</span> here!
           </h1>
-
-          <form className="transcript-form">
+          <form className="transcript-form" onSubmit={handleSubmit}>
             <div className="upload-box">
-
               <label htmlFor="transcript-file" className="upload-label">
-                {selectedFile ? selectedFile.name : "Choose your file "}
+                {selectedFile ? selectedFile.name : "Click or Drop File"}
               </label>
-
               <input 
                 type="file" 
                 id="transcript-file" 
@@ -37,13 +41,11 @@ const TranscriptUploadPage: React.FC = () => {
                 className="file-input"
                 onChange={handleFileChange}
               />
-
             </div>
             <button type="submit" className="submit-button">
               Submit
             </button>
           </form>
-          
         </div>
         <p className="transcript-instructions">
           This will let us extract your course history and grades to populate your academic plan.
