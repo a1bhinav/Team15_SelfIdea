667c4e2921e9f67c46a5b99b7f15c9865f6ca2b0
ananyasrivastav12 <ananyasrivas01@gmail.com>
2025-03-28 20:36:30 -0400

    updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import HelloWorld from "./components/HelloWorld";
-import PdfParser from "./components/PdfParser";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import TranscriptUploadPage from "./components/TranscriptUploadPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+
+  {
+    path: "/transcript",
+    element: <TranscriptUploadPage />,
+  }
+]);
+
 const App: React.FC = () => {
-  return (
-    <div>
-      <HelloWorld />
-      <PdfParser />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
